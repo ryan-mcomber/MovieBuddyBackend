86a8e2fa7a8b27a48c6a5ae2387ad405b5d8b54b
spencerc2009 <63590400+spencerc2009@users.noreply.github.com>
2021-10-15 14:48:40 -0400

    Returned users from suggest buddies

--- src/main/java/com/revature/service/BuddyResource.java
+++ src/main/java/com/revature/service/BuddyResource.java
@@ -3,6 +3,7 @@ package com.revature.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.revature.model.User;
@@ -10,13 +11,21 @@ import com.revature.model.User;
 @Service
 public class BuddyResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	public List<User> SuggestBuddies(User you) {
 		List<User> Buddies = new ArrayList<User>();
 		List<Integer> BuddyId = new ArrayList<Integer>();
 		
 		String genre = MovieResource.getMostPopularGenre(you.getId()); 
-		MovieResource.getBuddyByGenre(genre); 
-		return null;
+		BuddyId = MovieResource.getBuddyByGenre(genre);
+		for (int i = 0; i< BuddyId.size(); i++) {
+			if(you.getId() != BuddyId.get(i)) {//Discluding the user
+				Buddies.add(userService.findById(BuddyId.get(i))); //Get the full user object and add it to the buddies list
+			}
+		}
+		return Buddies;
 	}
 
 }
