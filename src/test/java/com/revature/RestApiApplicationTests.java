00a1b5ac6ee9f41161cb66d8da0ee4e6dcd498e7
ryan-mcomber <ryanmcom@hotmail.com>
2021-10-16 20:37:10 -0600

    testing stuff

--- src/test/java/com/revature/RestApiApplicationTests.java
+++ src/test/java/com/revature/RestApiApplicationTests.java
@@ -1,11 +1,14 @@
 package com.revature;
 
+import static org.junit.Assert.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import org.junit.Rule;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.revature.model.Movie;
@@ -14,24 +17,31 @@ import com.revature.service.MovieResource;
 @SpringBootTest
 class RestApiApplicationTests {
 	MovieResource mr = new MovieResource();
+
 	@Test
 	void contextLoads() {
 	}
 	@Test
 	void MovieGenreTest() {
 		
-		assertEquals("Drama",MovieResource.getMostPopularGenre(1));
+		assertEquals("Fantasy",MovieResource.getMostPopularGenre(1));
 	}
 	@Test
 	void MovieRecommendTest() {
 		
-		assertEquals(550,mr.getRecommendationId(1));
+		assertEquals(268,mr.getRecommendationId(1));
 	}
 	@Test
 	void MovieRecommendListTest() {
+		assertThrows(NullPointerException.class,()->{List<Movie> movieList =new ArrayList<Movie>();
+		movieList = mr.getMovieRecommendations(1);}
+		);
+	}
+	@Test
+	void GetMovieList() {
 		List<Movie> movieList =new ArrayList<Movie>();
-		//movieList = mr.getMovieRecommendations(1);
-		assertEquals(movieList.isEmpty(),true);
+		movieList = mr.getMovieList(1);
+		assertEquals(movieList.isEmpty(),false);
 	}
 
 }
