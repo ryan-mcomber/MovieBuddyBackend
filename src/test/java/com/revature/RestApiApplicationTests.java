efd14be16fbcb9024859b82629a03a35ce081b78
ryan-mcomber <ryanmcom@hotmail.com>
2021-10-16 20:37:07 -0600

    testing stuff

--- src/test/java/com/revature/RestApiApplicationTests.java
+++ src/test/java/com/revature/RestApiApplicationTests.java
@@ -2,9 +2,13 @@ package com.revature;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.revature.model.Movie;
 import com.revature.service.MovieResource;
 
 @SpringBootTest
@@ -16,12 +20,18 @@ class RestApiApplicationTests {
 	@Test
 	void MovieGenreTest() {
 		
-		assertEquals("Drama",mr.getMostPopularGenre(1));
+		assertEquals("Drama",MovieResource.getMostPopularGenre(1));
 	}
 	@Test
 	void MovieRecommendTest() {
 		
 		assertEquals(550,mr.getRecommendationId(1));
+	}
+	@Test
+	void MovieRecommendListTest() {
+		List<Movie> movieList =new ArrayList<Movie>();
+		//movieList = mr.getMovieRecommendations(1);
+		assertEquals(movieList.isEmpty(),true);
 	}
 
 }
